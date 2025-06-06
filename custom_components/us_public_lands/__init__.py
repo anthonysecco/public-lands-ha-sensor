61ff8f84035bdbf8d540ae2183cfc41ae992c5ee
Anthony Secco <155181953+anthonysecco@users.noreply.github.com>
2025-06-06 14:10:11 -0700

    Update __init__.py

--- custom_components/us_public_lands/__init__.py
+++ custom_components/us_public_lands/__init__.py
@@ -4,13 +4,20 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict):
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN].setdefault("sensors", [])
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "button")
+    )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    unload_sensor = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    unload_button = await hass.config_entries.async_forward_entry_unload(entry, "button")
+    return unload_sensor and unload_button
