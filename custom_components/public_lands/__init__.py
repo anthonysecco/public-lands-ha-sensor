67b7a5c626f16a7f7135ed0f8645186b137809b2
Anthony Secco <155181953+anthonysecco@users.noreply.github.com>
2025-06-10 08:16:04 -0700

    Update __init__.py
    Updated from
    hass.config_entries.async_forward_entry_setup
    to
    hass.config_entries.async_forward_entry_setups

--- custom_components/public_lands/__init__.py
+++ custom_components/public_lands/__init__.py
@@ -10,10 +10,10 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, "sensor")
     )
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "button")
+        hass.config_entries.async_forward_entry_setups(entry, "button")
     )
     return True
 
