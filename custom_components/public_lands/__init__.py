1f5818ab9537f179ba2f7bec7ea24263298b9b5c
Anthony Secco <155181953+anthonysecco@users.noreply.github.com>
2025-06-19 15:52:25 -0700

    Update __init__.py

--- custom_components/public_lands/__init__.py
+++ custom_components/public_lands/__init__.py
@@ -8,12 +8,11 @@ async def async_setup(hass: HomeAssistant, config: dict):
     hass.data[DOMAIN].setdefault("sensors", [])
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setups(entry, ["sensor", "button"])
     )
     return True
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     unload_sensor = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
